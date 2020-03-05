415148c6766087823e747f78f80f889dbefcd598
Mehdi Mosbah <mosbah.meh.didi@gmail.com>
2020-03-05 14:51:11 +0100

    rake vert

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
-  has_many :ingredients, through: :doses, dependent: :destroy
+  has_many :ingredients, through: :doses
 
   validates :name, uniqueness: true, presence: true
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Ingredient < ApplicationRecord
   has_many :doses
   validates :name, presence: true, uniqueness: true
